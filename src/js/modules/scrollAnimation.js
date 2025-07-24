b58bd081e43820a72dfb46d30b3a06fae33670d6
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-07-24 15:53:37 +0300

    fix: fix name svg

---
+++ src/js/modules/scrollAnimation.js
@@ -0,0 +1,39 @@
+export default function scrollAnimation(animationClass) {
+    const animItems = document.querySelectorAll(".gallery__img");
+
+    if (animItems.length > 0) {
+      window.addEventListener("scroll", animOnScroll);
+      function animOnScroll() {
+        for (let i = 0; i < animItems.length; i++) {
+          const animItem = animItems[i],
+            animItemHeight = animItem.offsetHeight,
+            animItemOffset = offset(animItem).top,
+            animStart = 10;
+
+          let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+          if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+          }
+
+          if (
+            scrollY > animItemOffset - animItemPoint &&
+            scrollY < animItemOffset + animItemHeight
+          ) {
+            animItem.classList.add(animationClass);
+          } else {
+            animItem.classList.remove(animationClass);
+          }
+        }
+      }
+      function offset(el) {
+        const rect = el.getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+      }
+      animOnScroll();
+    }
+  }
+
+
